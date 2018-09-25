a0fdd1003329e7a269c8370ca03877aabc966c6f
Ken-S-0114 <g031n071@s.iwate-pu.ac.jp>
2018-09-25 19:11:43 +0900

    first-commit

---
+++ GitHubSignup-Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubSignup-Example
+//
+//  Created by 佐藤賢 on 2018/09/25.
+//  Copyright © 2018年 佐藤賢. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
